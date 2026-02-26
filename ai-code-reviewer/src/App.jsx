971d61b2c8f330c0d76c01778e42d8291b8cf1a5
prashant00028 <savitaprashant005@gmail.com>
2026-02-26 18:46:27 +0530

    Initial commit

---
+++ ai-code-reviewer/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+
+  const [code, setCode] = useState("");
+  const [language, setLanguage] = useState("Java");
+  const [result, setResult] = useState("");
+  return (
+    <>
+    <Navbar/>
+    <div className='container'>
+      <h1>AI Code Reviewer</h1>
+      <p>Paste your code and get instant AI feedback.</p>
+
+      <CodeInput
+      code={code}
+      setcode={setCode}
+      language={language}
+      setLanguage={setLanguage}
+      />
+      
+      <ReviewResult result={result}/>
+
+    </div>
+    </>
+  )
+}
+
+export default App
